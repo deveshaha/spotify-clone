1f0717fccf2dd7034bc7c867d480afa5fcef92b4
deveshaha <devesh_hanumante@outlook.com>
2023-07-09 20:04:22 +0200

    addes UI Components

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,15 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div className="text-green-500">Main Content</div>
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mb-2">
+          <h1>
+            Welcome Back
+          </h1>
+        </div>
+      </Header>
+    </div>
   )
 }
